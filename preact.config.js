d1e24b0878d54cfe615420d6c24cfb550c2d58e4
Gregorein <gregbak@me.com>
2018-04-01 22:29:57 +0200

    some more things

--- preact.config.js
+++ preact.config.js
@@ -4,6 +4,8 @@ export default (config, env, helpers) => {
 	let {alias} = config.resolve;
 
 	alias.assets = path.resolve(__dirname, "src/assets");
+	alias.images = path.resolve(__dirname, "src/assets/images");
+	alias.icons = path.resolve(__dirname, "src/assets/icons");
 	alias.components = path.resolve(__dirname, "src/components");
 	alias.routes = path.resolve(__dirname, "src/routes");
 };
--- src/routes/policy-copyrights/index.js
+++ src/routes/policy-copyrights/index.js
@@ -8,14 +8,14 @@ const PolicyCopyrights = () => {
 	const d = new Date()
 	return (
 		<View class={styles.view}>
-			<h2>Policy &amp; Copyrights</h2>
+			<h1>Policy &amp; Copyrights</h1>
 			<p>This website and the content by <a href="/" title="Greg Bak Portfolio">Greg Bak</a> is licensed under a <a href="https://creativecommons.org/licenses/by/4.0/" title="Attribution 4.0 International">Attribution 4.0 International</a>, unless otherwise noted.</p>
-			<h3>Simply:</h3>
+			<h2>Simply:</h2>
 			<p>
 				You can <em>Share</em> — copy and redistribute the material in any medium or format.<br/>
 				You can <em>Adapt</em> — remix, transform, and build upon the material for any purpose, even commercially.
 			</p>
-			<h3>But.</h3>
+			<h2>But.</h2>
 			<p>
 				You <em>must</em> give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
 			</p>
