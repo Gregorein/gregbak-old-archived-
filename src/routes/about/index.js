ae365b4bffdcd3f1bf80a5599ff20448fd7721aa
Gregorein <gregbak@me.com>
2020-02-13 18:03:11 +0100

    updated about page

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -26,13 +26,21 @@ const About = () => (
           <h3 class={style.title}>earth arrival</h3>
         </li>
         <li>
-          <p class={style.date}>7.2011&mdash;2013</p>
-          <h3 class={style.title}>I was a concept 3d artist and animator. I worked on early alpha's character models and structures.</h3>
+          <p class={style.date}>7.2011&mdash;2013</p>          
+          <h3 class={style.title}>I was a concept 3d artist and animator.</h3>
+
+          <p class={style.description}>I worked on concept art and early alpha's models of characters and structures.<br />I documented source artwork and oversaw realisation of concept art.<br />I analised source-code and optimised animations accordingly</p>
+          <p class={style.tech}>C++, Python, Blender, Adobe Photoshop, Q3 Engine dev-tools, SVN</p>
+
           <a class={style.location} target="_blank" href="https://www.unvanquished.net">@Unvanquished Development <span>(https://www.unvanquished.net)</span></a>
         </li>
         <li>
           <p class={style.date}>6.2012&mdash;10.2013</p>
           <h3 class={style.title}>I was a volunteer UI translator (polish team).</h3>
+
+          <p class={style.description}>I translated UI from english to polish, maintained existing translations,<br />contacted Blender users to improve the quality of translations.</p>
+          <p class={style.tech}>Python, bash, git</p>
+
           <a class={style.location} target="_blank" href="https://www.blender.org">@Blender Institute <span>(https://www.blender.org)</span></a>
         </li>
         <li>
@@ -42,29 +50,76 @@ const About = () => (
         </li>
         <li>
           <p class={style.date}>4.2014&mdash;10.2014</p>
-          <h3 class={style.title}>I worked as a junior backend developer. I worked on plugins that patched-in missing functionality of database exports.</h3>
+          <h3 class={style.title}>I worked as a junior backend developer.</h3>
+
+          <p class={style.description}>I worked on plugins that patched-in missing functionality of database exports,<br />analised existing database structure and setup a prototyping sandbox<br />implemented export scripts with multiple file-types,<br />reworked UI to custom user workflow.</p>
+          <p class={style.tech}>Javascript, MySQL, PHP, Varico+EPESI, bash, VirtualBox</p>
+
           <a class={style.location} target="_blank" href="http://e-kwarta.pl">@Kwarta <span>(http://e-kwarta.pl)</span></a>
         </li>
         <li>
           <p class={style.date}>10.2014&mdash;10.2015</p>
-          <h3 class={style.title}>I worked freelance, as a designer and fullstack developer. I had to migrate previous CMS to a new Wordpress-based solution.</h3>
+          <h3 class={style.title}>I worked as a freelancea designer and fullstack developer.</h3>
+
+          <p class={style.description}>I migrated previous CMS to a new Wordpress-based solution,<br />I worked with client on a design that would fit client's upcoming rebranding,<br />I maintained backups and provided support for the system.</p>
+          <p class={style.tech}>Wordpress, PHP, Javascript, SASS, gulp, git</p>
+
           <a class={style.location} target="_blank" href="http://www.da.poznan.dominikanie.pl">@DA Dominikanie <span>(http://www.da.poznan.dominikanie.pl)</span></a>
+        </li>
+        <li>
+          <p class={style.date}>10.2014&mdash;10.2015</p>
+          <h3 class={style.title}>I worked as a freelancea designer and fullstack developer.</h3>
+
+          <p class={style.description}>I worked with client on a family-friendly design,<br />I maintained backups and provided support for the system.</p>
+          <p class={style.tech}>Wordpress, PHP, Javascript, SASS, gulp, git</p>
+
+          <a class={style.location} target="_blank" href="http://younglife.pl">@Young Life Poland <span>(http://younglife.pl)</span></a>
         </li>
         <li>
           <p class={style.date}>7.2015&mdash;5.2016</p>
           <h3 class={style.title}>I worked as a junior frontend developer in React.</h3>
+
+          <p class={style.description}>I worked on Wordpress websites for small corporate clients,<br />sliced & implemented UI designs.</p>
+          <p class={style.tech}>Wordpress, PHP, SASS, gulp, git, Trello</p>
+
+          <p class={style.description}>I worked on a single page CMS app in a small team,<br />sliced & implemented UI designs,<br />took part in code refactorisations.</p>
+
+          <p class={style.tech}>Javascript, React, SASS, Webpack, git, Jenkins CI, Trello</p>
           <a class={style.location} target="_blank" href="http://uxdream.com">@UX Dream <span>(http://uxdream.com)</span></a>
         </li>
         <li>
           <p class={style.date}>9.2016&mdash;1.2017</p>
-          <h3 class={style.title}>I worked freelance. I designed identification, the website, and developed a Wordpress solution for a web-based newspaper.</h3>
+          <h3 class={style.title}>I worked as a freelancea designer and fullstack developer.</h3>
+          <p class={style.description}>I designed identification and website for a web-based newspaper,<br />I maintained backups and provided support for the system.</p>
+          <p class={style.tech}>Wordpress, PHP, Javascript, SASS, gulp, git</p>
           <a class={style.location} target="_blank" href="http://fundamentum.com.pl">@Fundamentum <span>(http://fundamentum.com.pl)</span></a>
         </li>
         <li>
           <p class={style.date}>5.2017&mdash;5.2018</p>
-          <h3 class={style.title}>I worked as a fullstack web developer. I introduced ESNext, React and an update to the existing project lifecycle.</h3>
+          <h3 class={style.title}>I worked as a fullstack web developer</h3>
+
+          <p class={style.description}>I worked on Wordpress websites for small corporate clients,<br />sliced & implemented UI designs.</p>
+          <p class={style.tech}>Wordpress, PHP, SASS, gulp, git</p>
+
+          <p class={style.description}>I worked on a single page app search-engine app,<br />created and maintained UML diagrams for communication with backend,<br />sliced & implemented UI designs,<br />took part in code refactorisations.</p>
+          <p class={style.tech}>Javascript, React, SASS, Webpack, git</p>
+
+          <p class={style.description}>I worked on a single page app cryptocurrency trading market,<br />created and maintained UML diagrams for communication with backend,<br />sliced & implemented UI designs,<br />took part in code refactorisations.</p>
+          <p class={style.tech}>Typescript, React, SASS, Websocket, Webpack, git</p>
+
+          <p class={style.description}>I introduced ESNext, React and an update to the existing project lifecycle.</p>
+
           <a class={style.location} target="_blank" href="http://crafton.eu">@Crafton <span>(http://crafton.eu)</span></a>
         </li>
+        <li>
+          <p class={style.date}>10.2019&mdash;10.2020</p>
+          <h3 class={style.title}>I am working as a freelancea designer and fullstack developer.</h3>
+
+          <p class={style.description}>I migrated previous CMS to a light, frontend-only solution,<br />I worked with client on a design that would fit client's upcoming rebranding,<br />I implemented RWD and PWA,<br />I maintained backups and provided support for the system.</p>
+          <p class={style.tech}>Javascript, SASS, gulp, git</p>
+
+          <a class={style.location} target="_blank" href="http://www.dapoznan.pl">@DA Poznań <span>(http://www.dapoznan.pl)</span></a>
+        </li>        
         <li>
           <p class={style.date}>10.2017&mdash;now</p>
           <h3 class={style.title}>I'm finishing my Master of Engineering, Computer Science</h3>
@@ -72,7 +127,26 @@ const About = () => (
         </li>
         <li>
           <p class={style.date}>6.2018&mdash;now</p>
-          <h3 class={style.title}>I'm working as a regular frontend developer in React.</h3>
+          <h3 class={style.title}>I'm working as a frontend engineer in React.</h3>
+
+          <p class={style.description}>I've worked with my team on a Business Logic BL/CRM single-page app for an international client:<br />I've kept contact with client,<br />worked on project requirements analysis,<br />designed and implemented front-end,<br />created multiple alternative designs for UI,<br />managed code refactorisation,<br />created a complex in-browser WYSIWYG text editor,<br />implemented a PDF exporter for said editor</p>
+          <p class={style.tech}>Javascript, React, Redux, SASS, Express, Webpack, Gitlab CI/CD, Jira</p>
+
+          <p class={style.description}>I've worked in a team on a customer service portal for an international client with 50m+ users:<br />I've helped creating boilerpalte micro-services,<br />implemented a full, dynamic i18n support,<br />implemented a User-Agent/Platform-based code splitter for optimisation,<br />performed demos and workshops for client,<br />implemented full test coverage,<br />worked closely with UX/UI team on designs with RWD and accessibility in mind,<br />prepared documentation for business decisions.</p>
+          <p class={style.tech}>Javascript, Typescript, Jest, Handlebars.js, SASS, Express, Polyglot.js, Webpack, Eslint & Tslint, Docker, Kubernetes, Gitlab CI/CD, Jira, Confluence</p>
+
+          <p class={style.description}>Worked on a retirement pension calculation&management portal for an international client:<br />implemented UI with UX and accessibility in mind,<br />maintained high test coverage for UI library,<br />designed and built new views with mobile and tablet platforms as a target.</p>
+          <p class={style.tech}>Javascript, React, Jest&Enzyme, Sass, Webpack, Yarn, Redux w/Thunk, Eslint, Docker, Gitlab CI/CD, Jira, Confluence</p>
+
+          <p class={style.description}>I joined a team with an ongoing payment-provider project:<br />I've developed new functionalities and supported existing ones,<br />managed high test coverage,<br />sliced and implmeneted new views in a highly complex platform</p>
+          <p class={style.tech}>Javascript, Typescript, React, Jest&Enzyme, Sass, Webpack, Yarn, Eslint & Tslint, Docker, Gitlab CI/CD, Jira, Confluence</p>
+
+          <p class={style.description}>I joined an internal parking-place management app:<br/>I've implemented new functionalities,<br />rewritten all existing views and implemented a new, mobile friendly UI library,<br />implemented cutting-edge features like location-based night-mode.</p>
+          <p class={style.tech}>Javascript, React, Eslint, Hooks, Docker, Yarn, Gitlab CI/CD, Jira</p>
+
+          <p class={style.description}>I joined a rebranding project of a national marketplace provider,<br />I've rewritten existing views to apply a cutting-edge UI,<br />maintained and worked on a legacy-class code and libraries,<br />worked with UI team to re-work unsupported features<br />maintained feature-parity on a complex 9-domain project<br />written code for with a maximal browser coverage in mind.</p>
+          <p class={style.tech}>PHP, JQuery, Javascript, gulp, grunt, webpack, docker</p>
+
           <a class={style.location} target="_blank" href="https://sii.pl">@Sii_Polska <span>(https://sii.pl)</span></a>
         </li>
       </ul>
@@ -83,34 +157,53 @@ const About = () => (
         <li class={style.expert}>macOS</li>
         <li class={style.advanced}>UNIX</li>
         <li class={style.advanced}>Windows</li>
+        <li class={style.advanced}>bootloaders & kext patching</li>
+        <li class={style.intermediate}>emulation & virtualisation</li>
       </ul>
       <ul class={style.skills}>
         <h3>web stack</h3>
         <li class={style.expert}>SASS (with BEM)</li>
-        <li class={style.advanced}>React/Preact with Redux</li>
-        <li class={style.intermediate}>(with Saga, rx.js too)</li>
-        <li class={style.intermediate}>Typescript</li>
-        <li class={style.advanced}>ES7</li>
-        <li class={style.intermediate}>SQL</li>
+        <li class={style.advanced}>React/Preact</li>
+        <li class={style.advanced}>React hooks, Redux with Thunk</li>
+        <li class={style.intermediate}>Saga, Axios, RX.js</li>
+        <li class={style.advanced}>Typescript & ES7/ESNext, with linters</li>
+        <li class={style.advanced}>Webpack</li>
+        <li class={style.advanced}>Jest & Enzyme, Selenium & BrowserStack</li>
+        <li class={style.intermediate}>Node.js, Express</li>
+        <li class={style.intermediate}>MySQL/MSSQL, Mongo</li>
       </ul>
       <ul class={style.skills}>
         <h3>code</h3>
         <li class={style.advanced}>Golang</li>
         <li class={style.intermediate}>Python</li>
         <li class={style.intermediate}>C#, C++</li>
-        <li class={cn(style.intermediate, style.butwhytho)}>PHP</li>
+        <li class={style.intermediate}>Swift, Obj-C</li>
+        <li class={style.intermediate}>ReactNative</li>
+        <li class={style.intermediate}>bash/zsh</li>
+        <li class={cn(style.intermediate, style.butwhytho)}>PHP 7</li>
+      </ul>
+      <ul class={style.skills}>
+        <h3>project & engineering</h3>        
+        <li class={style.advanced}>Git, Mercurial, SVN</li>
+        <li class={style.advanced}>Agile, Scrum, Kanban</li>
+        <li class={style.intermediate}>Github, Gitlab, Bitbucket,</li>
+        <li class={style.intermediate}>Xcode, Vim w/g++ & gdb</li>
+        <li class={style.advanced}>UML & Application Architecture Design</li>
+        <li class={style.intermediate}>Redmine, Jenkins CI, Gitlab CI, Github CI</li>
       </ul>
       <ul class={style.skills}>
         <h3>graphics</h3>
-        <li class={style.expert}>Photoshop</li>
-        <li class={style.advanced}>Blender with Cycles</li>
+        <li class={style.expert}>Adobe Photoshop</li>
+        <li class={style.advanced}>Adobe Experience Designer</li>
+        <li class={style.intermediate}>Adobe Illustrator</li>
+        <li class={style.advanced}>Blender with Cycles/EEVEE render engines</li>
         <li class={style.learning}>Substance Painter</li>
         <li class={style.learning}>Marvelous Designer</li>
       </ul>
       <ul class={style.skills}>
         <h3>motion graphics</h3>
-        <li class={style.advanced}>Premiere Pro</li>
-        <li class={style.learning}>After Effects</li>
+        <li class={style.advanced}>Adobe Premiere Pro</li>
+        <li class={style.learning}>Adobe After Effects</li>
       </ul>
       <ul class={style.skills}>
         <h3>languages</h3>
