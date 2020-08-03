39a7890661617786d33a5ca8e848020b43d6b1d1
Grzegorz Bąk <gregbak@me.com>
2020-08-04 00:18:49 +0200

    migrated Mun to hooks

--- src/components/mun/index.js
+++ src/components/mun/index.js
@@ -1,84 +1,84 @@
-import {h, Component} from "preact"
+import {
+	useState,
+	useEffect,
+	useRef,
+} from "preact/hooks"
 
 import cn from "classnames"
 import style from "./style"
 
-class Mun extends Component {
-  state = {
-    visible: false,
-  }
-  textureLoader = new THREE.TextureLoader()
-  mun = undefined
+const Mun = () => {
+	const [visible, toggleVisibility] = useState(false)
+	const	textureLoader = new THREE.TextureLoader()
+	const container = useRef(null)
+	let mun, scene, camera, renderer
 
-  initScene = () => {
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000)
+	useEffect(() => {
+		initScene()
+		makeMun()
 
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true,
-    })
-    this.renderer.setSize(400, 400)
+		update()
+	}, [])
 
-    const light1 = new THREE.PointLight(0xffffff, 1.5)
-    light1.position.set(-800, 0, -150)
-    this.scene.add(light1)    
+	const initScene = () => {
+		scene = new THREE.Scene()
+		camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000)
 
-    const light2 = new THREE.PointLight(0xffffff, 1)
-    light2.position.set(300, 0, -350)
-    this.scene.add(light2)
+		renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true,
+		})
+		renderer.setSize(400, 400)
 
-    const container = document.querySelector("#mun")
-    container.appendChild(this.renderer.domElement)
-  }
+		const light1 = new THREE.PointLight(0xffffff, 1.5)
+		light1.position.set(-800, 0, -150)
+		scene.add(light1)    
 
-  makeMun = () => {
-    this.mun = new THREE.Mesh(
-      new THREE.SphereGeometry(
-        200, 32, 64,
-        0, Math.PI * 2,
-        0, Math.PI * 2
-      ),
-      new THREE.MeshPhysicalMaterial({
-        color: 0xfffdfa,
-        roughness: 1,
-        metalness: 0.4,
-        normalMap: this.textureLoader.load(
-          "assets/images/mun_n.png",
-          this.setState({visible: true}),
-        ),
-      })
-    )
+		const light2 = new THREE.PointLight(0xffffff, 1)
+		light2.position.set(300, 0, -350)
+		scene.add(light2)
 
-    this.mun.position.set(0, 0, -400)
-    this.scene.add(this.mun)
-  }
+		container.current.appendChild(renderer.domElement)
+	}
 
-  update = () => {
-    requestAnimationFrame(this.update)
+	const makeMun = () => {
+		mun = new THREE.Mesh(
+			new THREE.SphereGeometry(
+				200, 32, 64,
+				0, Math.PI * 2,
+				0, Math.PI * 2
+			),
+			new THREE.MeshPhysicalMaterial({
+				color: 0xfffdfa,
+				roughness: 1,
+				metalness: 0.4,
+				normalMap: textureLoader.load(
+					"assets/images/mun_n.png",
+					toggleVisibility(true),
+				),
+			})
+		)
 
-    this.mun.rotation.y -= 0.001
+		mun.position.set(0, 0, -400)
+		scene.add(mun)
+	}
 
-    this.renderer.render(this.scene, this.camera)
-  }
+	const update = () => {
+		requestAnimationFrame(update)
 
-  componentDidMount() {
-    this.initScene()
-    this.makeMun()
+		mun.rotation.y -= 0.001
 
-    this.update()
-  }
+		renderer.render(scene, camera)
+	}
 
-  render({}, {visible}) {
-    return (
-      <div
-        id="mun"
-        class={cn(style.mun, {
-          [style.visible]: visible,
-        })}
-        />
-    )
-  }
+	return (
+		<div
+			ref={container}
+			class={cn(style.mun, {
+				[style.visible]: visible,
+			})}
+			/>
+	)
 }
 
 export default Mun
