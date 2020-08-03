beebe4bb9b2acc97ae33f5105c570165ebb5f133
Grzegorz Bąk <gregbak@me.com>
2020-08-03 23:04:03 +0200

    migrated Filters to hooks

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,67 +1,51 @@
-import {
-  h,
-  Component,
-} from "preact"
 import {connect} from "react-redux"
 
 import {
-  resetFilters,
-  toggleFilter
+	resetFilters,
+	toggleFilter
 } from "actions/portfolio"
 
 import cn from "classnames"
 import style from "./style"
 
-class Filters extends Component {
-  renderFilters = () => {
-    const {
-      filters=[],
-      toggleFilter,
-    } = this.props
+const Filters = ({overrideClass, count, filters, resetFilters, toggleFilter}) => (
+	<div class={cn(overrideClass, style.wrapper)}>
+		<div
+			class={style.reset}
+			onClick={resetFilters}
+			>
+			reset tags
+		</div>
 
-    return filters.map(f => (
-      <div
-        class={cn(style.filter, {
-          [style.active]: f.active,
-        })}
-        onClick={() => toggleFilter(f.tag)}
-        >
-        {f.name}
-      </div>
-    ))
-  }
+		<div class={style.filters}>
+			{filters.map(f => (
+				<div
+					class={cn(style.filter, {
+						[style.active]: f.active,
+					})}
+					onClick={() => toggleFilter(f.tag)}
+					>
+					{f.name}
+				</div>
+			))}
+		</div>
 
-  render({overrideClass, count, filters, resetFilters}) {      
-    return (
-      <div class={cn(overrideClass, style.wrapper)}>
-        <div
-          class={style.reset}
-          onClick={resetFilters}
-          >
-          reset tags
-      </div>
+		<p class={cn(style.count, {
+			[style.noSelection]: count === undefined,
+		})}>
+			{count === undefined ? "showing all projects" : `found ${count} project${count !== 1 ? "s": ""}`}
+		</p>
+	</div>
+)
 
-      <div class={style.filters}>
-        {this.renderFilters()}
-      </div>
-
-      <p class={cn(style.count, {
-        [style.noSelection]: count === undefined,
-      })}>
-        {count === undefined ? "showing all projects" : `found ${count} project${count !== 1 ? "s": ""}`}
-      </p>
-    </div>
-    )
-  }
-}
 
 const stateProps = (state, props) => ({
-  count: state.portfolio.filteredCount,
-  filters: state.portfolio.filters,
+	count: state.portfolio.filteredCount,
+	filters: state.portfolio.filters,
 })
 const dispatchProps = (dispatch, props) => ({
-  resetFilters: () => dispatch(resetFilters()),
-  toggleFilter: tag => dispatch(toggleFilter(tag)),
+	resetFilters: () => dispatch(resetFilters()),
+	toggleFilter: tag => dispatch(toggleFilter(tag)),
 })
 
 export default connect(stateProps, dispatchProps)(Filters)
